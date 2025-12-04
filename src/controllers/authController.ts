26e76caaa5a6ab01c4a51a762a7585518f973251
José Nascimento <josearmandonascimento20@gmail.com>
2025-12-04 03:47:27 -0300

    Update authController.ts

--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -1,5 +1,6 @@
 
 import { Request, Response } from 'express';
+import { AuthService } from '../services/authService';
 import { loginSchema } from '../schemas/authSchemas';
 import { ZodError } from 'zod';
 
